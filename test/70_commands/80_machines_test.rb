8437d6631aff66da6f01ce3ca809d891f221864e
delano <delano@solutious.com>
2009-03-29 13:14:17 -0400

    Re-organized commands tests

--- test/70_commands/80_machines_test.rb
+++ test/70_commands/80_machines_test.rb
@@ -1,23 +1,7 @@
-require File.join(File.dirname(__FILE__), 'helper')
 
 module Rudy::Test
-  class Machines < Test::Unit::TestCase
-    @@logger = STDERR #StringIO.new
-    @@rmach = Rudy::Machines.new(:logger => @@logger)
-    @@rgroup = Rudy::Groups.new(:logger => @@logger)
-    
-    def setup
-      
-    end
-    
-    def teardown
-      #if @logger.is_a?(StringIO)
-      #  @logger.rewind
-      #  output = @logger.read
-      #  puts $/, "Rudy output:".bright, output unless output.empty?
-      #end
-    end
-    
+  class Machines
+
     xcontext "Machines in current group" do
       
       should "(00) have current machine group" do

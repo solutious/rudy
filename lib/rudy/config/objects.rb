47db6751e117d6248d8dd991e42b0ec6cb3582b8
delano <delano@solutious.com>
2009-04-10 11:08:38 -0400

    Moved forced_hash to caesars 0.5.3

--- lib/rudy/config/objects.rb
+++ lib/rudy/config/objects.rb
@@ -19,49 +19,11 @@ class Rudy::Config
   end
   
   class Routines < Caesars
+    
+    forced_hash :create
+    forced_hash :destroy
+    forced_hash :restore
+    forced_hash :mount
 
-    def create(*args, &b)
-      hash_handler(:create, *args, &b)
-    end
-    def destroy(*args, &b)
-      hash_handler(:destroy, *args, &b)
-    end
-    def restore(*args, &b)
-      hash_handler(:restore, *args, &b)
-    end
-    def mount(*args, &b)
-      hash_handler(:mount, *args, &b)
-    end
-
-    #
-    # Force the specified keyword to always be treated as a hash. 
-    # Example:
-    #
-    #     startup do
-    #       disks do
-    #         create "/path/2"         # Available as hash: [action][disks][create][/path/2] == {}
-    #         create "/path/4" do      # Available as hash: [action][disks][create][/path/4] == {size => 14}
-    #           size 14
-    #         end
-    #       end
-    #     end
-    #
-    def hash_handler(caesars_meth, *args, &b)
-      # TODO: Move to caesars
-      return @caesars_properties[caesars_meth] if @caesars_properties.has_key?(caesars_meth) && args.empty? && b.nil?
-      return nil if args.empty? && b.nil?
-      return method_missing(caesars_meth, *args, &b) if args.empty?
-
-      caesars_name = args.shift
-
-      prev = @caesars_pointer
-      @caesars_pointer[caesars_meth] ||= Caesars::Hash.new
-      hash = Caesars::Hash.new
-      @caesars_pointer = hash
-      b.call if b
-      @caesars_pointer = prev
-      @caesars_pointer[caesars_meth][caesars_name] = hash 
-      @caesars_pointer = prev
-    end
   end
 end

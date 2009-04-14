6ef923bc1e9b29855e94673aad142c15f091c4ce
delano <delano@solutious.com>
2009-04-14 13:34:46 -0400

    rudy-ec2 images is done

--- lib/rudy/aws/ec2/image.rb
+++ lib/rudy/aws/ec2/image.rb
@@ -1,21 +1,102 @@
 
 
 module Rudy::AWS
+  class EC2::Image < Storable
+    @@sformat = "   -> %8s; %12s; %12s; %12s; %8s"
+    
+    field :awsid
+    field :owner
+    field :aki
+    field :ari
+    field :state
+    field :arch
+    field :visibility
+    field :location
+    field :kind
+    
+    def liner_note
+      info = @location
+      # Highlight "debian-5" in /dir/debian-5.manifest.xml
+      #info = info.split(/\//) 
+      #info[-1].gsub!(/(.+?)((\.img)?\.manifest\.xml)/) { |m,n| ($1 || "").bright << $2 }
+      #info = info.join('/')
+      "%s (%s)" % [self.awsid.bright, info] 
+    end
+        
+    def to_s(with_title=false)
+      lines = []
+      lines << liner_note
+      if self.available?
+        p = public? ? "public" : "private" 
+        k, r = @aki || 'aki-unknown', @ari || 'ari-unknown'
+        lines << @@sformat % %w{arch owner aki ari visibility} if with_title
+        lines << @@sformat % [@arch, @owner, k, r, p]
+      end
+      lines.join($/)
+    end
+    
+    def inspect
+      lines = []
+      lines << liner_note
+      field_names.each do |key|
+        next unless self.respond_to?(key)
+        val = self.send(key)
+        lines << sprintf(" %22s: %s", key, (val.is_a?(Array) ? val.join(', ') : val))
+      end
+      lines.join($/)
+    end
+    
+    def available?; @state && @state == "available"; end
+    def public?; @visibility && @visibility == 'public'; end
+    def private?; !public? end
+    
+  end
+  
   module EC2
     
     class Images
       include Rudy::AWS::ObjectBase
+      include Rudy::AWS::EC2::Base
       
+      def list(owner=[], image_ids=[], executable_by=[], &each_image)
+        images = list_as_hash(owner, image_ids, executable_by)
+        images &&= images.values
+        images
+      end
       
-      def list()
+      def list_as_hash(owner=[], image_ids=[], executable_by=[], &each_image)
+        owner &&= [owner].flatten.compact
+        image_ids &&= [image_ids].flatten.compact
+        executable_by &&= [executable_by].flatten.compact
+        
+        # Remove dashes from aws account numbers
+        owner &&= owner.collect { |o| o.tr('-', '') }
+        # If we got Image objects, we want just the IDs.
+        # This method always returns an Array.
+        image_ids = objects_to_image_ids(image_ids)
+        
         opts = {
-          :image_id => [],
-          :owner_id => [],
-          :executable_by => []
+          :owner_id => owner || [],
+          :image_id => image_ids || [],
+          :executable_by => executable_by || []
         }
-        ret = @aws.describe_images_by_owner(opts) || []
-        puts ret.to_yaml
-        []
+        
+        response = execute_request({}) { @ec2.describe_images(opts) }
+        
+        return nil unless response['imagesSet'].is_a?(Hash)  # No instances 
+      
+        resids = []
+        images = {}
+        response['imagesSet']['item'].each do |res|      
+          resids << res['reservationId']
+          img = Images.from_hash(res)
+          images[img.awsid] = img
+        end
+        
+        images.each_value { |image| each_image.call(image) } if each_image
+        
+        images = nil if images.empty? # Don't return an empty hash
+        images
       end
       
       # +id+ AMI ID to deregister (ami-XXXXXXX)
@@ -24,7 +105,7 @@ module Rudy::AWS
         opts = {
           :image_id => id
         }
-        ret = @aws.deregister_image(opts)
+        ret = @ec2.deregister_image(opts)
         puts ret.to_yaml
         true
       end
@@ -35,12 +116,53 @@ module Rudy::AWS
         opts = {
           :image_location => path
         }
-        ret = @aws.register_image(opts)
+        ret = @ec2.register_image(opts)
         puts ret.to_yaml
         true
       end
+      
+      #     imageOwnerId: "203338247012"
+      #     kernelId: aki-a71cf9ce
+      #     ramdiskId: ari-a51cf9cc
+      #     imageState: available
+      #     imageId: ami-dd34d3b4
+      #     architecture: i386
+      #     isPublic: "false"
+      #     imageLocation: solutious-rudy-us/debian-squeeze-m1.small-v5.manifest.xml
+      #     imageType: machine
+      def Images.from_hash(h)
+        img = Rudy::AWS::EC2::Image.new
+        img.owner = h['imageOwnerId']
+        img.aki = h['kernelId']
+        img.ari = h['ramdiskId']
+        img.state = h['imageState']
+        img.awsid = h['imageId']
+        img.arch = h['architecture']
+        img.visibility = h['isPublic'] == 'true' ? 'public' : 'private'
+        img.location = h['imageLocation']
+        img.kind = h['imageType']
+        img
+      end
+      
+      
+    private
+    
+      # * +img_ids+ an Array of images IDs (Strings) or Image objects.
+      # Note: This method removes nil values and always returns an Array.
+      # Returns an Array of image IDs. 
+      def objects_to_image_ids(img_ids)
+        img_ids = [img_ids].flatten    # Make sure it's an Array
+        img_ids = img_ids.collect do |img|
+          next if img.nil? || img.to_s.empty?
+          if !img.is_a?(Rudy::AWS::EC2::Image) && !Rudy.is_id?(:image, img)
+            raise %Q("#{img}" is not an image ID or object)
+          end
+          img.is_a?(Rudy::AWS::EC2::Image) ? img.awsid : img
+        end
+        img_ids
+      end
+      
     end
-    
-    
+
   end
 end
--- lib/rudy/cli/aws/ec2/images.rb
+++ lib/rudy/cli/aws/ec2/images.rb
@@ -4,109 +4,112 @@ module Rudy; module CLI;
 module AWS; module EC2;
   
   class Images < Rudy::CLI::Base
-    
 
+   #def create_images_valid?
+   #  puts "Make sure the machine is clean. I don't want archive no crud!"
+   #  switch_user("root")
+   #  
+   #    raise "No EC2 .pem keys provided" unless has_pem_keys?
+   #    raise "No SSH key provided for #{@global.user}!" unless has_keypair?
+   #    raise "No SSH key provided for root!" unless has_keypair?(:root)
+   #  true
+   #end
+   #
+   #
+   #def prepare_images
+   #  # TODO: Avail hooks for clean an instance
+   #  # Clean off Rudy specific crap. 
+   #end
+   #
+   #
+   #def create_images
+   #  puts "Creating image from #{machine_group}"
+   #  
+   #  # ~/.rudy, /etc/motd, history -c, /etc/hosts, /var/log/rudy*
+   #  
+   #  execute_check(:medium)
+   #  
+   #  
+   #  machine_list = @ec2.instances.list(machine_group)
+   #  machine = machine_list.values.first  # NOTE: Only one machine per group, for now...
+   #
+   #  raise "There's no machine running in #{machine_group}" unless machine
+   #  raise "The primary machine in #{machine_group} is not in a running state" unless machine[:aws_state] == 'running'
+   #  
+   #  puts "The new image will be based on #{machine_group}_01"
+   #  
+   #  @option.account ||= @config.accounts.aws.accountnum
+   #  
+   #  # TODO: Don't reset global here
+   #  unless @option.account
+   #    puts "Enter your 12 digit Amazon account number:"
+   #    @global.accountnum = gets.chomp
+   #  end
+   #  
+   #  unless @option.image_name
+   #    puts "Enter the image name:"
+   #    @option.image_name = gets.chomp
+   #  end
+   #  
+   #  unless @option.bucket_name
+   #    puts "Enter the S3 bucket that will store the image:"
+   #    @option.bucket_name = gets.chomp
+   #  end
+   #  
+   #  unless @option.print
+   #    puts "Copying .pem keys to /mnt (they will not be included in the AMI)"
+   #    scp_command machine[:dns_name], keypairpath, @global.user, @global.cert, "/mnt/"
+   #    scp_command machine[:dns_name], keypairpath, @global.user, @global.privatekey, "/mnt/"
+   #  end
+   #  
+   #  ssh do |session|
+   #    session.exec!("touch /root/firstrun")
+   #  end
+   #  
+   #  puts "Starting bundling process...".bright
+   #  puts ssh_command(machine[:dns_name], keypairpath, @global.user, "ec2-bundle-vol -r i386 -p #{@option.image_name} -k /mnt/pk-*pem -c /mnt/cert*pem -u #{@option.account}", @option.print)
+   #  puts ssh_command(machine[:dns_name], keypairpath, @global.user, "ec2-upload-bundle -b #{@option.bucket_name} -m /tmp/#{@option.image_name}.manifest.xml -a #{@global.accesskey} -s #{@global.secretkey}", @option.print)
+   #
+   #  @ec2.images.register("#{@option.bucket_name}/#{@option.image_name}.manifest.xml") unless @option.print
+   #end
+   #
+   #def deregister
+   #  ami = @argv.first
+   #  raise "You must supply an AMI ID (ami-XXXXXXX)" unless ami
+   #  puts "Deregistering AMI: #{ami}"
+   #  
+   #  exit unless Annoy.are_you_sure?
+   #  
+   #  if @ec2.images.deregister(ami)
+   #    puts "Done!"
+   #  else
+   #    puts "There was an unknown problem!"
+   #  end
+   #  
+   #end
+    
+    
+    def images_valid?
+      if @option.owner == 'self'
+        raise "AWS_ACCOUNT_NUMBER not set" unless @@global.accountnum 
+        @option.owner = @@global.accountnum 
+      end
+      
+      true  
+    end
     def images
-      @ec2.images.list.each do |img|
-        print_image img
+      
+      @option.owner ||= 'amazon'
+      puts "Images owned by #{@option.owner.bright}" unless @argv.awsid
+      rimages = Rudy::AWS::EC2::Images.new(@@global.accesskey, @@global.secretkey)
+      images = rimages.list(@option.owner, @argv) || []
+      images.each do |img|
+        puts @@global.verbose > 0 ? img.inspect : img.dump(@@global.format)
       end
+      puts "No images"
     end
-    
-    def create_images_valid?
-      puts "Make sure the machine is clean. I don't want archive no crud!"
-      switch_user("root")
-      
-        raise "No EC2 .pem keys provided" unless has_pem_keys?
-        raise "No SSH key provided for #{@global.user}!" unless has_keypair?
-        raise "No SSH key provided for root!" unless has_keypair?(:root)
-      true
-    end
-    
-    
-    def prepare_images
-      # TODO: Avail hooks for clean an instance
-      # Clean off Rudy specific crap. 
-    end
-    
-    
-    def create_images
-      puts "Creating image from #{machine_group}"
-      
-      # ~/.rudy, /etc/motd, history -c, /etc/hosts, /var/log/rudy*
-      
-      execute_check(:medium)
-      
-      
-      machine_list = @ec2.instances.list(machine_group)
-      machine = machine_list.values.first  # NOTE: Only one machine per group, for now...
 
-      raise "There's no machine running in #{machine_group}" unless machine
-      raise "The primary machine in #{machine_group} is not in a running state" unless machine[:aws_state] == 'running'
-      
-      puts "The new image will be based on #{machine_group}_01"
-      
-      @option.account ||= @config.accounts.aws.accountnum
-      
-      # TODO: Don't reset global here
-      unless @option.account
-        puts "Enter your 12 digit Amazon account number:"
-        @global.accountnum = gets.chomp
-      end
-      
-      unless @option.image_name
-        puts "Enter the image name:"
-        @option.image_name = gets.chomp
-      end
-      
-      unless @option.bucket_name
-        puts "Enter the S3 bucket that will store the image:"
-        @option.bucket_name = gets.chomp
-      end
-      
-      unless @option.print
-        puts "Copying .pem keys to /mnt (they will not be included in the AMI)"
-        scp_command machine[:dns_name], keypairpath, @global.user, @global.cert, "/mnt/"
-        scp_command machine[:dns_name], keypairpath, @global.user, @global.privatekey, "/mnt/"
-      end
-      
-      ssh do |session|
-        session.exec!("touch /root/firstrun")
-      end
-      
-      puts "Starting bundling process...".bright
-      puts ssh_command(machine[:dns_name], keypairpath, @global.user, "ec2-bundle-vol -r i386 -p #{@option.image_name} -k /mnt/pk-*pem -c /mnt/cert*pem -u #{@option.account}", @option.print)
-      puts ssh_command(machine[:dns_name], keypairpath, @global.user, "ec2-upload-bundle -b #{@option.bucket_name} -m /tmp/#{@option.image_name}.manifest.xml -a #{@global.accesskey} -s #{@global.secretkey}", @option.print)
-
-      @ec2.images.register("#{@option.bucket_name}/#{@option.image_name}.manifest.xml") unless @option.print
-    end
     
-    def deregister
-      ami = @argv.first
-      raise "You must supply an AMI ID (ami-XXXXXXX)" unless ami
-      puts "Deregistering AMI: #{ami}"
-      
-      exit unless Annoy.are_you_sure?
-      
-      if @ec2.images.deregister(ami)
-        puts "Done!"
-      else
-        puts "There was an unknown problem!"
-      end
-      
-    end
-    
-    
-    def print_image(img)
-      puts 
-      puts "Image: #{img[:aws_id].bright}"
-      img.each_pair do |key, value|
-         printf(" %22s: %s#{$/}", key, value) if value
-       end
-      puts
-    end
-    
-    
-      
     end
 
 

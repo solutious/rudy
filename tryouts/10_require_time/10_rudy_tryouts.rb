c9e8b4f635fdd36ec8e5065f318e770836284f8f
delano <delano@solutious.com>
2009-06-05 18:10:21 -0400

    Added tryouts for rudy.rb

---
+++ tryouts/10_require_time/10_rudy_tryouts.rb
@@ -0,0 +1,47 @@
+
+LIB_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+
+group "Rudy Require-Time"
+library :rudy, LIB_DIR
+
+tryout "Initialization" do
+  setup do
+  end
+
+  drill "version matches gemspec", Rudy::VERSION.to_s
+  drill             "has sysinfo", Rudy.sysinfo
+  drill       "debug is disabled", Rudy.debug?
+  drill       "quiet is disabled", Rudy.quiet?
+  drill    "auto-yes is disabled", Rudy.yes?
+  
+  drill "debug can be enabled" do
+    Rudy.enable_debug
+    Rudy.debug?
+  end
+  
+  drill "debug can be disabled" do
+    Rudy.disable_debug
+    Rudy.debug?
+  end
+  
+  
+end
+
+
+dreams "Initialization" do
+  dream "version matches gemspec" do
+    require 'rubygems' unless defined?(Gem)
+    eval( File.read(File.join(RUDY_HOME, 'rudy.gemspec')))
+    output @spec.version.to_s
+  end
+  
+  dream           "has sysinfo", SysInfo, :class
+  dream     "debug is disabled", false
+  dream     "quiet is disabled", false
+  dream  "auto-yes is disabled", false
+  
+  dream  "debug can be enabled", true
+  dream "debug can be disabled", true
+end
+
+

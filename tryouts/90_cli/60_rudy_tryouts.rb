13d22be81c4b22984e457936864633cc930298a8
delano <delano@solutious.com>
2009-06-06 01:02:59 -0400

    Added dreams for rudy cli tryouts

--- tryouts/90_cli/60_rudy_tryouts.rb
+++ tryouts/90_cli/60_rudy_tryouts.rb
@@ -1,10 +1,10 @@
 
-RUDY_HOME = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+RUDY_HOME = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 
 group "Rudy CLI"
 command :rudy, File.join(RUDY_HOME, 'bin', 'rudy')
 
-dreams File.join(GYMNASIUM_HOME, '90_cli')
+dreams File.join(GYMNASIUM_HOME, '90_cli', '60_rudy_dreams.rb')
 tryout "rudy machines" do
   drill "no machines, no args", :machines
   ##drill "startup", :startup

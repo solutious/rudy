0d905bb613732154ce7701521639daa2832d9915
delano <delano@solutious.com>
2009-08-01 13:25:34 -0400

    Rocking Rudy::Backups#list with tryouts

--- lib/rudy/backups.rb
+++ lib/rudy/backups.rb
@@ -5,8 +5,20 @@ module Rudy
     
     extend self
     extend Rudy::Metadata::ClassMethods 
+    include Rudy::Huxtable
     extend Rudy::Huxtable
     
+    # Returns the most recent backup object for the given path
+    def get(path)
+      tmp = Rudy::Backup.new path
+      backups = Rudy::Backups.list :path => path
+      return nil unless backups.is_a?(Array) && !backups.empty?
+      backups.first
+    end
+    
+    def from_hash(h)
+      Rudy::Backup.from_hash h
+    end
   end
   
 end
---
+++ tryouts/30_metadata/63_backup_list_tryouts.rb
@@ -0,0 +1,38 @@
+
+group "Metadata"
+library :rudy, 'lib'
+
+Gibbler.enable_debug if Tryouts.verbose > 3
+  
+tryout "List Backups" do
+  
+  set :sample_time, Time.now.utc
+  
+  setup do
+    Rudy.enable_debug
+    Rudy::Huxtable.update_config          # Read config files
+    global = Rudy::Huxtable.global
+    akey, skey, region = global.accesskey, global.secretkey, global.region
+    Rudy::Metadata.connect akey, skey, region
+    Rudy::AWS::EC2.connect akey, skey, region
+    Rudy::Backup.new(1, '/any/path1', :created => sample_time).save :replace
+    Rudy::Backup.new(2, '/any/path2', :created => sample_time).save :replace
+  end
+  
+  clean do
+    Rudy::Backup.new(1, '/any/path1', :created => sample_time).destroy
+    Rudy::Backup.new(2, '/any/path2', :created => sample_time).destroy
+  end
+  
+  dream :class, Array
+  dream :empty?, false
+  dream :size, 2
+  drill "can list available backups" do
+    Rudy::Backups.list
+  end
+  
+  
+end
+
+
+

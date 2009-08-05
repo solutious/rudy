95d078ac95e837af964f7da2b45ac303ad1018d3
delano <delano@solutious.com>
2009-08-05 01:07:02 -0400

    Release 0.9.1 (Show us teeth!)
         ,--------,
       ,--------,' |
      |    _     | |
      |  .' `.   | |
      |  `._.'   | |
      |          |,',-,-,-,-,-,-,-,-,-,-,-,-,-,-==,
      |__________\------------------------------=='

--- lib/rudy.rb
+++ lib/rudy.rb
@@ -42,7 +42,7 @@ module Rudy
     unless defined?(MAJOR)
       MAJOR = 0.freeze
       MINOR = 9.freeze
-      TINY  = 0.freeze
+      TINY  = 1.freeze
     end
     def self.to_s; [MAJOR, MINOR, TINY].join('.'); end
     def self.to_f; self.to_s.to_f; end

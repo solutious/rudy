e9b0115dc63f5ae22dc2d6c1d7517a5556420907
delano <delano@solutious.com>
2009-08-18 12:25:20 -0400

    Added "rudy keypairs"

--- examples/windows.rb
+++ examples/windows.rb
@@ -8,12 +8,13 @@ defaults do
   color true
   environment :test
   role :windows
-  #region :'eu-west-1'
+  region :'eu-west-1'
 end
 
 machines do
   region :'us-east-1' do
     ami 'ami-de4daab7'               # Amazon Windows Server 2003 (US)
+    size 'm1.small'
   end
   region :'eu-west-1' do
     ami 'ami-f98fa78d'               # Rudy Windows (EU)
@@ -21,6 +22,7 @@ machines do
   env :test do
     role :windows do
       os :win32
+
     end
   end
 end

fd0480f6785f2bbf324a32a0da874265b6af720c
delano <delano@solutious.com>
2010-01-16 18:57:20 -0500

    Release 0.9.6 (more responsive)

--- lib/rudy.rb
+++ lib/rudy.rb
@@ -43,8 +43,8 @@ module Rudy
     unless defined?(MAJOR)
       MAJOR = 0.freeze
       MINOR = 9.freeze
-      TINY  = 5.freeze
-      PATCH  = '004'.freeze
+      TINY  = 6.freeze
+      PATCH  = '001'.freeze
     end
     def self.to_s; [MAJOR, MINOR, TINY, PATCH].join('.'); end
     def self.to_f; self.to_s.to_f; end
--- rudy.gemspec
+++ rudy.gemspec
@@ -1,7 +1,7 @@
 @spec = Gem::Specification.new do |s|
   s.name = "rudy"
   s.rubyforge_project = 'rudy'
-  s.version = "0.9.5.004"
+  s.version = "0.9.6.001"
   s.summary = "Rudy: Not your grandparents' EC2 deployment tool."
   s.description = s.summary
   s.author = "Delano Mandelbaum"

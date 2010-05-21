c95c84a8689bb872bf2c8e2e37c9e23a9fe8b693
delano <delano@solutious.com>
2010-05-21 18:33:33 -0400

    FIXED: uninitialized constant Rye::Box::Huxtable error when running "rudy m -U"

--- lib/rudy/routines/handlers/host.rb
+++ lib/rudy/routines/handlers/host.rb
@@ -100,7 +100,7 @@ module Rudy; module Routines; module Handlers;
           if hn != :default
             original_user = rset.user
             rset.switch_user 'root'
-            rset.add_key Huxtable.user_keypairpath('root')
+            rset.add_key ::Rudy::Huxtable.user_keypairpath('root')
             hn = self.stash.name if hn == :rudy
             self.quietly { hostname(hn) }
             rset.switch_user original_user
